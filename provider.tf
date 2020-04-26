99802331638555d5858b6654446c3219c2e81d85
Adjamilton Junior <jr@ieee.org>
2020-04-26 13:05:31 -0300

    Update comment

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "aws" {
   shared_credentials_file = "/home/jr/.aws/credentials"
   profile                 = "pessoal"
   
-  # O Cognito só está disponível em N. Virginia (US East)
+  # O Cognito só está disponível na região de N. Virginia (US East)
   region = "us-east-1"
 }
